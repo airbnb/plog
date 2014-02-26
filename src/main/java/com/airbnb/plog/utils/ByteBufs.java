bc74237f970ed0092f03fccbccddbad37dfa2d13
Pierre Carrier <pierre@gcarrier.fr>
2014-02-25 17:48:12 -0800

    ByteBufs: don't bother hiding constructor
    Breaks code coverage, no point.

--- src/main/java/com/airbnb/plog/utils/ByteBufs.java
+++ src/main/java/com/airbnb/plog/utils/ByteBufs.java
@@ -3,10 +3,6 @@ package com.airbnb.plog.utils;
 import io.netty.buffer.ByteBuf;
 
 public final class ByteBufs {
-    private ByteBufs() {
-        // cannot be instantiated
-    }
-
     public static byte[] toByteArray(ByteBuf buf) {
         final int length = buf.readableBytes();
         final byte[] payload = new byte[length];
