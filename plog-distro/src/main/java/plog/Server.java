1ba2d12e476394ca650caaa4160ae8a804ac28b0
Pierre Carrier <pierre@gcarrier.fr>
2014-05-07 01:04:17 -0700

    plog-distro: convenient launchers
    Allows the shorter `java -cp plog.jar plog.Server`.

---
+++ plog-distro/src/main/java/plog/Server.java
@@ -0,0 +1,9 @@
+package plog;
+
+import com.airbnb.plog.server.PlogServer;
+
+public class Server {
+    public static void main(String[] args) {
+        PlogServer.main(args);
+    }
+}
---
+++ plog-distro/src/main/java/plog/Stress.java
@@ -0,0 +1,11 @@
+package plog;
+
+import com.airbnb.plog.stress.PlogStress;
+
+import java.net.SocketException;
+
+public class Stress {
+    public static void main(String[] args) throws SocketException {
+        PlogStress.main(args);
+    }
+}
